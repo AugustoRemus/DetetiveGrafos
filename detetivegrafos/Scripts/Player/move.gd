01ad9deaf3d0a17e828c346b99f016ec58c421e6
Augusto Renostro Remus <augusto.remus@estudante.uffs.edu.br>
2025-06-14 21:44:20 -0300

    AnimacaoAndarPlayer

--- detetivegrafos/Scripts/Player/move.gd
+++ detetivegrafos/Scripts/Player/move.gd
@@ -1,8 +1,10 @@
 extends Node
-
 @export var player: CharacterBody2D
+@export var sprite: Sprite2D
 
 
+
+#este script cuida da movimentação e animação de movimentação do jogador
 
 
 #region moveVars
@@ -14,12 +16,22 @@ var direcao : Vector2
 #quando tiver upgrades testar a logica para pegar o certo
 var Speed: float = GlobalPlayer.velocidadeMovimentoPlayer
 
-
+#arrumar depois pra ver como fica mais agradavel
 @export_range(0.0001, 0.9999) var aceleration: float
 @export_range(0.0001, 0.9999) var fric : float
 
 
 #endregion
+
+
+#region animationVars
+#contador do bob
+var bob_time := 0.0
+var bob_speed := GlobalPlayer.bobSpeedPlayer
+var bob_height := 3.0
+
+#endregion
+
 
 func _init() -> void:
 
@@ -29,6 +41,10 @@ func _init() -> void:
 
 func _physics_process(delta):
 	_move()
+	_animation()
+	
+	#botar um teste para o player nao conseguir sair do mapa
+	
 	player.move_and_slide()
 	
 
@@ -49,4 +65,15 @@ func _move():
 	player.velocity.x = lerp(player.velocity.x, direcao.normalized().x * Speed, fric)
 	player.velocity.y = lerp(player.velocity.y, direcao.normalized().y * Speed, fric)
 	
-	
+
+
+func _animation():
+	#personagem esta se movendo
+	#vai gerar uma onda e vai definindo a posição do sprite
+	#basiada nesse calculo, muita doidera e matematica
+	if direcao != Vector2.ZERO:
+		bob_time += bob_speed
+		sprite.position.y = sin(bob_time) * bob_height
+	else:
+		#esta parado entao vai resetando aos poucos
+		sprite.position.y = lerp(sprite.position.y, 0.0, 0.2)
--- detetivegrafos/Scripts/global_player.gd
+++ detetivegrafos/Scripts/global_player.gd
@@ -2,5 +2,9 @@ extends Node
 
 
 
-var velocidadeMovimentoPlayer = 100
+var velocidadeMovimentoPlayer := 100.0
+#se aumentar a velocidade diminuir isso para a animação 
+#ficar mais rapida
+var bobSpeedPlayer := 0.3
+
 var zoomPlayer = Vector2(1,1)
