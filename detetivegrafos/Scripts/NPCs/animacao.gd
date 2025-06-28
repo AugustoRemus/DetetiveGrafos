a97161755fa5e4d7ce585a6163aa17bd3e21e8a2
Augusto Renostro Remus <augusto.remus@estudante.uffs.edu.br>
2025-06-27 22:46:03 -0300

    comentario e teste mov/anima NPCs

--- detetivegrafos/Scripts/NPCs/animacao.gd
+++ detetivegrafos/Scripts/NPCs/animacao.gd
@@ -3,13 +3,14 @@ extends Node
 @export var spriteNPC: Sprite2D
 
 #pode variar se o npc for mais rapido, adicionar no resource
+#no bob_speed
 var bob_time := 0.0
 var bob_speed := 0.4
 var bob_height := 3.0
 
 
 
-
+#passa a velocity do character do npc
 func _animation(direcao):
 	#vai ver se o npc esta se movendo
 	if direcao != Vector2.ZERO:
