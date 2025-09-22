1e6d5d0198d67417afef24d2998f9554a6889d4a
Augusto Renostro Remus <augusto.remus@estudante.uffs.edu.br>
2025-09-22 03:22:24 -0300

    SombraBasica

--- detetive-grafos-(copy)/Scripts/NPCs/spriteNPCs.gd
+++ detetive-grafos-(copy)/Scripts/NPCs/spriteNPCs.gd
@@ -3,18 +3,26 @@ extends Sprite2D
 #character base
 @onready var base_np_cs: CharacterBody2D = $".."
 @onready var silhueta: Sprite2D = $Silhueta
+@onready var sombra: Sprite2D = $sombra
 
 #pode variar se o npc for mais rapido, adicionar no resource
 #no bob_speed
 var bob_time := 0.0
 var bob_speed := 0.4
 var bob_height := 3.0
+
 var base_offset
+
+var base_sombra_scale
+var base_sombra_offset
+
 
 
 func _ready() -> void:
 	bob_speed = base_np_cs.resourceNPC.RbobSpeed
 	base_offset = offset
+	base_sombra_scale = sombra.scale
+	base_sombra_offset = sombra.offset
 
 
 func _physics_process(delta: float) -> void:
@@ -24,11 +32,17 @@ func _physics_process(delta: float) -> void:
 	#vai ver se o npc esta se movendo
 	if base_np_cs.velocity != Vector2.ZERO:
 		bob_time += bob_speed
-		offset.y = base_offset.y + sin(bob_time) * bob_height
-		silhueta.offset.y = base_offset.y + sin(bob_time) * bob_height
-	else:
 		
+		
+		var bob = sin(bob_time) * bob_height
+		offset.y = base_offset.y + bob
+		silhueta.offset.y = base_offset.y + bob
+		
+		#ajustando a sombra
+		var scale_factor = 1.0 - (bob / (bob_height * 2.0))
+		sombra.scale = base_sombra_scale * clamp(scale_factor, 0.95, 1.05)
+	else:
+		#volta
 		offset.y = lerp(offset.y, base_offset.y, 0.2)
 		silhueta.offset.y = lerp(offset.y, base_offset.y, 0.2)
-		
-		
+		sombra.scale = sombra.scale.lerp(base_sombra_scale, 0.2)
--- detetive-grafos-(copy)/Scripts/Player/SpritePlayer.gd
+++ detetive-grafos-(copy)/Scripts/Player/SpritePlayer.gd
@@ -1,27 +1,42 @@
 extends Sprite2D
 
+@onready var sombra: Sprite2D = $sombra
 
 #region animationVars
 #contador do bob
 var bob_time := 0.0
 var bob_speed := GlobalPlayer.bobSpeedPlayer
 var bob_height := 3.0
+
 var base_offset
+
+var base_sombra_scale: Vector2
+var base_sombra_offset: Vector2
+
 #endregion
 
 
 func _ready():
 	
 	base_offset = offset
+	base_sombra_scale = sombra.scale
+	base_sombra_offset = sombra.offset
 
 func _animation(direcao):
 	#personagem esta se movendo
 	#vai gerar uma onda e vai definindo a posição do sprite
 	#basiada nesse calculo, muita doidera e matematica
-	
 	if direcao != Vector2.ZERO:
-		bob_time += bob_speed 
-		offset.y = base_offset.y + sin(bob_time) * bob_height
-	else:
+		bob_time += bob_speed
 		
+		
+		var bob = sin(bob_time) * bob_height
+		offset.y = base_offset.y + bob
+		
+		#ajustando a sombra
+		var scale_factor = 1.0 - (bob / (bob_height * 2.0))
+		sombra.scale = base_sombra_scale * clamp(scale_factor, 0.95, 1.05)
+	else:
+		#volta
 		offset.y = lerp(offset.y, base_offset.y, 0.2)
+		sombra.scale = sombra.scale.lerp(base_sombra_scale, 0.2)
